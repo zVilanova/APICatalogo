0a591f5cd4eb9e4ca074475c53085787e40bee40
Leonardo Guedes <guedesleonardo022@gmail.com>
2025-04-10 17:18:46 -0300

    Applying migrations and implementing data annotations

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace APICatalogo.Models;
 
@@ -9,7 +10,13 @@ public class Categoria
         Produtos = new Collection<Produto>();
     }
     public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+    
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
     public ICollection<Produto>? Produtos { get; set; }
 }
