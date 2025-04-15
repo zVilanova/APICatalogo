ea273f8fe0222115e74034053a4622bf7c6cc99c
Leonardo Guedes <guedesleonardo022@gmail.com>
2025-04-15 20:29:26 -0300

    Optimized code and resolved cyclic reference issue between Produtos and Categorias entities

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -20,13 +20,14 @@ namespace APICatalogo.Controllers
         [HttpGet("produtos")]
         public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
         {
-            return _context.Categorias.Include(p => p.Produtos).ToList();
+            //return _context.Categorias.Include(p => p.Produtos).ToList();
+            return _context.Categorias.Include(p => p.Produtos).Where(c => c.CategoriaId <= 5).ToList();
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<Categoria>> Get() 
         {
-            return _context.Categorias.ToList();
+            return _context.Categorias.AsNoTracking().ToList();
         }
 
         [HttpGet("{id:int}", Name = "ObterCategoria")]
--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,11 +1,15 @@
+using System.Text.Json.Serialization;
 using APICatalogo.Context;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+        options.JsonSerializerOptions
+            .ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
